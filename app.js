389af8d4e4ecc8c93b6ee8efac1014b6d904191c
Pechdanai.Nine <nineza321@gmail.com>
2025-12-17 13:35:55 +0700

    add hiragana api

--- app.js
+++ app.js
@@ -3,9 +3,82 @@ const app = express();
 const port = 3001;
 
 // Define the Hello World API endpoint
-app.get('/api/hello', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
+
+app.get('/hiragana', (req, res) => {
+    res.json([
+	// A-row
+	{ char: "あ", romaji: "a" },
+	{ char: "い", romaji: "i" },
+	{ char: "う", romaji: "u" },
+	{ char: "え", romaji: "e" },
+	{ char: "お", romaji: "o" },
+
+	// KA-row
+	{ char: "か", romaji: "ka" },
+	{ char: "き", romaji: "ki" },
+	{ char: "く", romaji: "ku" },
+	{ char: "け", romaji: "ke" },
+	{ char: "こ", romaji: "ko" },
+
+	// SA-row
+	{ char: "さ", romaji: "sa" },
+	{ char: "し", romaji: "shi" },
+	{ char: "す", romaji: "su" },
+	{ char: "せ", romaji: "se" },
+	{ char: "そ", romaji: "so" },
+
+	// TA-row
+	{ char: "た", romaji: "ta" },
+	{ char: "ち", romaji: "chi" },
+	{ char: "つ", romaji: "tsu" },
+	{ char: "て", romaji: "te" },
+	{ char: "と", romaji: "to" },
+
+	// NA-row
+	{ char: "な", romaji: "na" },
+	{ char: "に", romaji: "ni" },
+	{ char: "ぬ", romaji: "nu" },
+	{ char: "ね", romaji: "ne" },
+	{ char: "の", romaji: "no" },
+
+	// HA-row
+	{ char: "は", romaji: "ha" },
+	{ char: "ひ", romaji: "hi" },
+	{ char: "ふ", romaji: "fu" },
+	{ char: "へ", romaji: "he" },
+	{ char: "ほ", romaji: "ho" },
+
+	// MA-row
+	{ char: "ま", romaji: "ma" },
+	{ char: "み", romaji: "mi" },
+	{ char: "む", romaji: "mu" },
+	{ char: "め", romaji: "me" },
+	{ char: "も", romaji: "mo" },
+
+	// YA-row
+	{ char: "や", romaji: "ya" },
+	{ char: "ゆ", romaji: "yu" },
+	{ char: "よ", romaji: "yo" },
+
+	// RA-row
+	{ char: "ら", romaji: "ra" },
+	{ char: "り", romaji: "ri" },
+	{ char: "る", romaji: "ru" },
+	{ char: "れ", romaji: "re" },
+	{ char: "ろ", romaji: "ro" },
+
+	// WA-row
+	{ char: "わ", romaji: "wa" },
+	{ char: "を", romaji: "wo" },
+
+	// N
+	{ char: "ん", romaji: "n" },
+]);
+}
+);
 
 // Start the server
 app.listen(port, () => {
